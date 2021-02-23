a7394c5ee12b6f4c97c99cff2d0bb752b861f4ff
OxanaPeregud <ox.peregud@gmail.com>
2021-02-23 08:17:29 +0300

    solution: Done new class 'DateFormat'

---
+++ src/main/java/com/rakovets/course/design/practice/solid/Pizza/Order/DateFormat.java
@@ -0,0 +1,11 @@
+package com.rakovets.course.design.practice.solid.Pizza.Order;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormat {
+
+    public static String localDatePattern(LocalDateTime localDate) {
+        return localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+    }
+}
