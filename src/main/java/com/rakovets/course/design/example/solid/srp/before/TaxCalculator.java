e00fca6fbd8217d048f499ff539460d0e7b2ee39
Dmitry Rakovets <dmitryrakovets@gmail.com>
2021-03-12 05:59:00 +0300

    refactor: delete unused class for theme 'SOLID'

--- src/main/java/com/rakovets/course/design/example/solid/srp/before/TaxCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.rakovets.course.design.example.solid.srp.before;
-
-import com.rakovets.course.design.example.solid.srp.after.Account;
-
-public class TaxCalculator {
-    public TaxTable calculateTaxes(Account account, int year) {
-        return new TaxTable();
-    }
-}
