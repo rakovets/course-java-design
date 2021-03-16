439c8c6a27b280817941155697181598e29fce3c
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-03-16 20:24:08 +0300

    fix

--- src/main/java/com/rakovets/course/design/practice/solid/pizza/service/PizzaOrderService.java
+++ src/main/java/com/rakovets/course/design/practice/solid/pizza/service/PizzaOrderService.java
@@ -114,7 +114,6 @@ public class PizzaOrderService {
                 throw new UserInputException(choice);
             } catch (UserInputException ex) {
                 ex.printStackTrace();
-                sc.nextInt();
             }
         }
     }
