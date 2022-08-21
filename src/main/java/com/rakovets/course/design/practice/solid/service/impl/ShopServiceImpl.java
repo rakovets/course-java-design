3f73be85ed227208c49ac3edc755f1e2d4aae9bd
Anastasia <taa.cheshire@gmail.com>
2022-08-21 23:56:47 +0200

    feat: edit ShopServiceImpl

--- src/main/java/com/rakovets/course/design/practice/solid/service/impl/ShopServiceImpl.java
+++ src/main/java/com/rakovets/course/design/practice/solid/service/impl/ShopServiceImpl.java
@@ -3,32 +3,39 @@ package com.rakovets.course.design.practice.solid.service.impl;
 import com.rakovets.course.design.practice.solid.model.Pizza;
 import com.rakovets.course.design.practice.solid.model.ingredient.Ingredient;
 import com.rakovets.course.design.practice.solid.service.ShopService;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 
-public class ShopServiceImpl implements ShopService {
+public final class ShopServiceImpl implements ShopService {
+    @Contract("_, _ -> new")
     @Override
-    public Pizza makePizzaFromTheChef(String name, Collection<Ingredient> ingredients) {
+    public @NotNull Pizza makePizzaFromTheChef(String name, Collection<Ingredient> ingredients) {
         return new Pizza(name, ingredients);
     }
 
+    @Contract("_, _ -> new")
     @Override
-    public Pizza makePizzaMargaritta(String name, Collection<Ingredient> ingredients) {
+    public @NotNull Pizza makePizzaMargaritta(String name, Collection<Ingredient> ingredients) {
         return new Pizza(name, ingredients);
     }
 
+    @Contract("_, _ -> new")
     @Override
-    public Pizza makePizzaPepperoni(String name, Collection<Ingredient> ingredients) {
+    public @NotNull Pizza makePizzaPepperoni(String name, Collection<Ingredient> ingredients) {
         return new Pizza(name, ingredients);
     }
 
+    @Contract("_, _ -> new")
     @Override
-    public Pizza makePizzaPesto(String name, Collection<Ingredient> ingredients) {
+    public @NotNull Pizza makePizzaPesto(String name, Collection<Ingredient> ingredients) {
         return new Pizza(name, ingredients);
     }
 
+    @Contract("_, _ -> new")
     @Override
-    public Pizza makePizzaVegetarian(String name, Collection<Ingredient> ingredients) {
+    public @NotNull Pizza makePizzaVegetarian(String name, Collection<Ingredient> ingredients) {
         return new Pizza(name, ingredients);
     }
 }
